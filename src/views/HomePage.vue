6e1ee851d0f726291cf5448503235d192b73ca33
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-02-01 14:07:28 +0200

    little login

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,34 +1,39 @@
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton  } from '@ionic/vue';
+
+
+
+
+</script>
+
 <template>
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Profile</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Profile</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Do you have an account? </strong>
+        <p>Login or register</p>
+        <ion-button size="large" href="/login">Login</ion-button>
+        <ion-button size="large" href="/register">Register</ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-</script>
-
 <style scoped>
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +49,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,90 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <form novalidate @submit.prevent="loginHandle">
+        <ion-list>
+          <ion-item>
+            <ion-input
+                label="Username"
+                labelPlacement="stacked"
+                v-model="login"
+                name="username"
+                type="text"
+                autocapitalize="off"
+                required
+            ></ion-input>
+          </ion-item>
+
+          <ion-item>
+            <ion-input
+                labelPlacement="stacked"
+                label="Password"
+                v-model="password"
+                name="password"
+                type="password"
+                required
+            ></ion-input>
+          </ion-item>
+        </ion-list>
+
+        <ion-row responsive-sm class="ion-padding">
+          <ion-col>
+            <ion-button type="submit" expand="block">
+              Login
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button
+                href="/register"
+                color="light"
+                expand="block">
+              Signup
+            </ion-button>
+          </ion-col>
+        </ion-row>
+      </form>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonCol, IonRow} from "@ionic/vue";
+import { ref } from "vue"
+
+const login = ref("niga");
+const password = ref("niga");
+
+const emit = defineEmits();
+
+function loginHandle(event: Event){
+   if (password.value === login.value && login.value === "admin"){
+
+   }
+}
+
+</script>
+
+<style scoped>
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+</style>
