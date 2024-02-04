36656817b2b2690c3446310a8166b95ced642646
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-02-04 10:16:25 +0200

    photos var delete, only last

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,11 +1,10 @@
-import {onMounted, ref, watch} from 'vue';
-import {Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera';
-import {Directory, Filesystem} from '@capacitor/filesystem';
-import {Preferences} from '@capacitor/preferences';
-import {isPlatform} from '@ionic/vue';
-import {Capacitor} from '@capacitor/core';
+import { onMounted, ref } from 'vue';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
 
-const photos = ref<UserPhoto[]>([]);
 const lastImage = ref<UserPhoto>({
                   filepath: "http://www.gravatar.com/avatar?d=mm&s=140",
                   webviewPath: "http://www.gravatar.com/avatar?d=mm&s=140" });
@@ -29,94 +28,73 @@ export const usePhotoGallery = () => {
             });
             if (photo) {
                 const fileName = Date.now() + '.jpeg';
-                const savedFileImage = await savePicture(photo, fileName);
-                lastImage.value = savedFileImage;
-                photos.value = [savedFileImage, ...photos.value];
+                lastImage.value = await savePicture(photo, fileName);
             }
         } catch (error) {
             console.error('Error capturing photo:', error);
         }
     };
+    const convertBlobToBase64 = (blob: Blob) =>
+        new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+            reader.readAsDataURL(blob);
+        });
+
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        let base64Data: string | Blob;
+        if (isPlatform('hybrid')) {
+            const file = await Filesystem.readFile({
+                path: photo.path!,
+            });
+            base64Data = file.data;
+        } else {
+            const response = await fetch(photo.webPath!);
+            const blob = await response.blob();
+            base64Data = (await convertBlobToBase64(blob)) as string;
+        }
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+
+        if (isPlatform('hybrid')) {
+            return {
+                filepath: savedFile.uri,
+                webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+            };
+        } else {
+            return {
+                filepath: fileName,
+                webviewPath: photo.webPath,
+            };
+        }
+    };
+
+    const loadSaved = async () => {
+        const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+        const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+
+        if (!isPlatform('hybrid')) {
+            for (const photo of photosInPreferences) {
+                const file = await Filesystem.readFile({
+                    path: photo.filepath,
+                    directory: Directory.Data,
+                });
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+            }
+        }
+    };
+
+    onMounted(loadSaved);
 
     return {
-        photos,
         lastImage,
         takePhoto,
     };
 };
 
-const convertBlobToBase64 = (blob: Blob) =>
-    new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onerror = reject;
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.readAsDataURL(blob);
-    });
-
-const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-    let base64Data: string | Blob;
-    // "hybrid" will detect mobile - iOS or Android
-    if (isPlatform('hybrid')) {
-        const file = await Filesystem.readFile({
-            path: photo.path!,
-        });
-        base64Data = file.data;
-    } else {
-        // Fetch the photo, read as a blob, then convert to base64 format
-        const response = await fetch(photo.webPath!);
-        const blob = await response.blob();
-        base64Data = (await convertBlobToBase64(blob)) as string;
-    }
-    const savedFile = await Filesystem.writeFile({
-        path: fileName,
-        data: base64Data,
-        directory: Directory.Data,
-    });
-
-    if (isPlatform('hybrid')) {
-        // Display the new image by rewriting the 'file://' path to HTTP
-        return {
-            filepath: savedFile.uri,
-            webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-        };
-    } else {
-        // Use webPath to display the new image instead of base64 since it's
-        // already loaded into memory
-        return {
-            filepath: fileName,
-            webviewPath: photo.webPath,
-        };
-    }
-};
-
-const cachePhotos = () => {
-    Preferences.set({
-        key: PHOTO_STORAGE,
-        value: JSON.stringify(photos.value),
-    });
-};
-
-watch(photos, cachePhotos);
-
-const loadSaved = async () => {
-    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
-    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
-
-    // If running on the web...
-    if (!isPlatform('hybrid')) {
-        for (const photo of photosInPreferences) {
-            const file = await Filesystem.readFile({
-                path: photo.filepath,
-                directory: Directory.Data,
-            });
-            // Web platform only: Load the photo as base64 data
-            photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
-        }
-    }
-
-    photos.value = photosInPreferences;
-};
-
-onMounted(loadSaved);
